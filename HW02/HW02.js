3ee3afe14ff980e14e9d0dadcd5a9bf64b585ab3
20574895 <u20574895@tuks.co.za>
2022-05-03 14:30:02 +0200

    Initial commit

---
+++ HW02/HW02.js
@@ -0,0 +1,59 @@
+let movies1 = [
+    {
+        id: 1,
+        title: "Narnia: The Lion, the Witch and the Wardrobe",
+        director: "Andrew Adamson and Michael Apted",
+        runtime: "2h 30m",
+        release_year: 2005,
+        description: "While playing, Lucy and her siblings find a wardrobe that lands them in a mystical place called Narnia. Here they realise that it was fated and they must now unite with Aslan to defeat an evil queen.",
+        poster_url: "https://images.app.goo.gl/xvhM2FVQCdAs44LZA",
+        cinema_number: 1,
+        ticket_price: "R30",
+        tickets_in_cart: ""
+    }
+]
+
+let movies2 = [
+    {
+        id: 2,
+        title: "The Matrix",
+        director: "Lana Wachowski, Lilly Wachowski",
+        runtime: "2h 27m",
+        release_year: 1999,
+        description: "Thomas Anderson, a computer programmer, is led to fight an underground war against powerful computers who have constructed his entire reality with a system called the Matrix.",
+        poster_url: "https://images.app.goo.gl/FpCPkUytf9F7NYTh6",
+        cinema_number: 2,
+        ticket_price: "R40",
+        tickets_in_cart: ""  
+    }
+]
+
+let movies3 = [
+    {
+        id: 3,
+        title: "The Notebook",
+        director: "Nick Cassavetes",
+        runtime: "2h 4m",
+        release_year: 2004,
+        description: "Duke reads the story of Allie and Noah, two lovers who were separated by fate, to Ms Hamilton, an old woman who suffers from dementia, on a daily basis out of his notebook.",
+        poster_url: "https://images.app.goo.gl/7MvwYzfHEUo6HrYc7",
+        cinema_number: 3,
+        ticket_price: "R45",
+        tickets_in_cart: ""  
+    }
+]
+
+let movies4 = [
+    {
+        id: 4,
+        title: "The Proposal",
+        director: "Anne Fletcher",
+        runtime: "1h 48m",
+        release_year: 2009,
+        description: "When New York editor Margaret faces deportation, she convinces her assistant Andrew to marry her in return for a promotion. However, when she visits his hometown, it changes her in many ways.",
+        poster_url: "https://images.app.goo.gl/RtrQ5BCECV6E4s3z6",
+        cinema_number: 4,
+        ticket_price: "R35",
+        tickets_in_cart: ""  
+    }
+]
